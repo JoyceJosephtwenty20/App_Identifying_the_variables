8e62422709522f7ae2c831e7929e24ebe1e3496f
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-16 14:19:45 -0400

    app_commit1

---
+++ src/AppIdentifyingVariables.java
@@ -0,0 +1,15 @@
+public class AppIdentifyingVariables {
+    public static void main(String[] argu){
+       String cuName = "Kenya";
+       String cuPopulation = "49.7 million";
+       String cuCapital = "Nairobi";
+       double oneUsd = 103.84;
+
+
+       System.out.println("Hay Mom and Dad!" + "\nHere are some interesting details about the first country I'm visiting.");
+       System.out.println("Name: " + cuName + "\nPopulation Size: " + cuPopulation + "\nCapital: " +cuCapital + "\nCurency: One U.S Dollar equals $" + oneUsd + " Kenyan Shillings!!");
+       System.out.println("I'am so excited to visit the " + cuCapital + " National Park and feel the giraffes!! Talk later.");
+
+
+    }
+}
